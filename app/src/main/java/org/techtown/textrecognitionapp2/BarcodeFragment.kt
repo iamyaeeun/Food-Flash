fca6f70451491eca2e9e047baa180cbc9ddb4b75
허예은 <20211415@sungshin.ac.kr>
2022-08-06 22:52:11 +0900

    yaeeun notend commit

--- app/src/main/java/org/techtown/textrecognitionapp2/BarcodeFragment.kt
+++ app/src/main/java/org/techtown/textrecognitionapp2/BarcodeFragment.kt
@@ -1,17 +1,20 @@
 package org.techtown.textrecognitionapp2
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.techtown.textrecognitionapp2.databinding.FragmentBarcodeBinding
-import org.techtown.textrecognitionapp2.org.techtown.textrecognitionapp2.MainActivity
+import org.techtown.textrecognitionapp2.MainActivity
+import java.util.*
 
-class BarcodeFragment : Fragment() {
-
+class BarcodeFragment : Fragment(){
     var mainActivity: MainActivity? = null
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
@@ -27,7 +30,8 @@ class BarcodeFragment : Fragment() {
         val binding = FragmentBarcodeBinding.inflate(inflater, container, false)
         binding.btnNext.setOnClickListener { mainActivity?.goExpirationDate() }
         binding.btnBackToMain.setOnClickListener { mainActivity?.goMain() }
+        //추가
+        binding.btnBarcode.setOnClickListener {mainActivity?.startBarcodeReader()}
         return binding.root
     }
-
 }
--- app/src/main/java/org/techtown/textrecognitionapp2/MainActivity.kt
+++ app/src/main/java/org/techtown/textrecognitionapp2/MainActivity.kt
@@ -1,14 +1,35 @@
-package org.techtown.textrecognitionapp2.org.techtown.textrecognitionapp2
+package org.techtown.textrecognitionapp2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.speech.tts.TextToSpeech
+import android.speech.tts.TextToSpeech.OnInitListener
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import org.techtown.textrecognitionapp2.*
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import com.google.zxing.integration.android.IntentIntegrator
 import org.techtown.textrecognitionapp2.databinding.ActivityMainBinding
 import org.techtown.textrecognitionapp2.databinding.FragmentInformationBinding
+import java.util.*
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),OnInitListener {
+    var barcode: String? = null
+    var tts: TextToSpeech? = null
+    var expirationDate: String? = null
+    //var imageView: ImageView? = null
+    var textView: TextView? = null
+    var textView2: TextView? = null
+    var imageBitmap: Bitmap? = null
+    var msg: String? = null
 
     val binding_main by lazy { ActivityMainBinding.inflate(layoutInflater) }
     val binding by lazy { FragmentInformationBinding.inflate(layoutInflater) }
@@ -16,12 +37,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding_main.root)
+        //imageView = findViewById<ImageView>(R.id.image_view)
+        textView= findViewById<TextView>(R.id.textView)
+        textView2=findViewById<TextView>(R.id.textView2)
+        tts = TextToSpeech(this, this)
 
         val data:MutableList<Memo> = loadData()
         var adapter = CustomAdapter()
         adapter.listData = data
         binding.recyclerView.adapter = adapter
-
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
 
         setMainFragment()
@@ -73,5 +97,109 @@ class MainActivity : AppCompatActivity() {
             data.add(memo)
         }
         return data
+    }
+
+    //바코드 촬영 코드
+    fun startBarcodeReader(){
+        IntentIntegrator(this).initiateScan()
+    }
+    //유통기한 촬영 후 텍스트 인식 코드 추가
+    fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+    fun detectTextFromImage() {
+        val inputImage = InputImage.fromBitmap(imageBitmap!!, 0)
+        val textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+        textRecognizer.process(inputImage)
+            .addOnSuccessListener { text -> displayTextFromImage(text) }
+            .addOnFailureListener { e ->
+                Toast.makeText(this@MainActivity, "Error" + e.message, Toast.LENGTH_SHORT).show()
+                Log.d("Error: ", e.message!!)
+            }
+    }
+    fun displayTextFromImage(text: Text) {
+        val blockList = text.textBlocks
+        if (blockList.size == 0) {
+            msg = "글자를 인식하지 못했습니다. 다시 찍어주세요."
+            textView2!!.text = msg //Toast.makeText(this,"No Text Found in image.",Toast.LENGTH_SHORT).show();
+            speakExpirationDate()
+        } else {
+            for (block in text.textBlocks) {
+                msg = block.text
+                textView2!!.text = msg
+                speakExpirationDate()
+            }
+        }
+    }
+    //바코드, 유통기한 처리 코드(if문:유통기한 사진인 경우, else문:바코드인 경우)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode== REQUEST_IMAGE_CAPTURE){
+            val extras = data!!.extras
+            imageBitmap = extras!!["data"] as Bitmap?
+            //imageView!!.setImageBitmap(imageBitmap)
+        }
+        else{
+            val result= IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+            if(result!=null){
+                barcode=result.contents
+                Toast.makeText(this,barcode, Toast.LENGTH_LONG).show()
+                speakBarcode()
+            }
+            else{
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
+    }
+    //바코드 음성 안내 코드 추가
+    fun speakBarcode(){
+        val text: String? = barcode
+        tts!!.setPitch(0.6.toFloat())
+        tts!!.setSpeechRate(0.1.toFloat())
+        if(text==null){
+            tts!!.speak("식품을 찍어주시면 유통기한을 음성 안내 해드립니다.", TextToSpeech.QUEUE_FLUSH,null,"id2")
+        }
+        else{
+            tts!!.speak("이 식품은 "+text+"입니다. 유통기한을 촬영하려면 오른쪽 하단에 있는 버튼을, 메인 화면으로 돌아가려면 왼쪽 하단에 있는 버튼을 눌러주세요", TextToSpeech.QUEUE_FLUSH, null, "id1")
+        }
+    }
+    //유통기한 음성 안내 코드 추가
+    fun speakExpirationDate(){
+        val text: String? = msg
+        tts!!.setPitch(0.6.toFloat())
+        tts!!.setSpeechRate(0.1.toFloat())
+        if(text==null){
+            tts!!.speak("", TextToSpeech.QUEUE_FLUSH,null,"id4")
+        }
+        else{
+            tts!!.speak("", TextToSpeech.QUEUE_FLUSH, null, "id3")
+        }
+    }
+
+    override fun onDestroy() {
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        super.onDestroy()
+    }
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts!!.setLanguage(Locale.KOREA)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported")
+            } else {
+                //btn_speak.setEnabled(true);
+                speakBarcode() //speakOut();
+            }
+        } else {
+            Log.e("TTS", "Initilization Failed!")
+        }
+    }
+    companion object {
+        const val REQUEST_IMAGE_CAPTURE = 1
     }
 }
