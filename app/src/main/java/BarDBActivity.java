3955f0a61565bbddcae1cbbd5a0a46f92ed9cf82
im-genie <108739432+im-genie@users.noreply.github.com>
2022-08-09 10:31:16 +0900

    barcodeDB 최종

--- app/src/main/java/BarDBActivity.java
+++
@@ -1,7 +0,0 @@
-//데이터베이스 정보
-
-public class BarDBActivity {
-    public static final String COL_BARCODE = "BRCD_NO"; //바코드(기본키)
-    public static final String COL_BARNAME = "PRDT_NM"; //제품명
-    public static final String _TABLENAME3 = "barcodeData"; //테이블명
-}
